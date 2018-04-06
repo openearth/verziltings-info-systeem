56c06b9bdcd84b54e55e2e1a69ecdd6858e11327
matthijs schaap <matthijs.schaap@deltares.nl>
2018-04-06 10:24:47 +0200

    added readme, php database creation, simplified upload form, get db data with waterbody ID

---
+++ php/README.md
@@ -0,0 +1,6 @@
+# verziltings-info-systeem - PHP data handler
+
+- makes a sqlite database
+- adds form submitted data to database
+- copies files into "files" folder for download later
+- can be asked to return list of files based on Waterbody ID
--- php/verzilting.php
+++ php/verzilting.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
 /*
 * php database manager
 * Deltares, Matthijs Schaap, 4-2018
@@ -6,32 +8,77 @@
 * adds documents to database
 * queries database for documents with area
 */
+
+//If submit == true we have data from the form
 if(isset($_POST['submit'])){
     $db = new Dbase;
     $postData = $_POST;
-    //check for valid information
+    $uploadDir = "D:/Applications/Webserver/Apache24/htdocs/verzilting/files/";
+    $tmp_dir = ini_get('upload_tmp_dir') ? ini_get('upload_tmp_dir') : sys_get_temp_dir();
         
     //save document
-    $link = 'savedPath';
-        
-    //add to database    
-    $db -> sqlite_addDocumentToDb($postData, $link);
+    if ( 0 < $_FILES['document']['error'] ) {
+        echo 'Error: ' . $_FILES['file']['error'] . '<br>';
+        die;
+    }
+    if(!isset ($_FILES['document']["name"]))
+    {
+        echo 'Error: No File uploaded <br>';
+        die;
+    }
     
-    //all is well
-    echo $postData['title'];
+    $target_fileName = basename($_FILES["document"]["name"]);
+    $fileType = strtolower(pathinfo($target_fileName, PATHINFO_EXTENSION));
+    
+    // Allow certain file formats
+    if($fileType != "doc" &&
+       $fileType != "docx" &&
+       $fileType != "pdf" &&
+       $fileType != "ppt" &&
+       $fileType != "pptx") {
+        echo "You tried to upload a ".$fileType."-file. ";
+        echo "However only DOC, DOCX, PDF, PPT & PPTX files are allowed.";
+        die;
+    }
+    
+    //should move uploaded file, but since that does not work, we use copy
+    $checkMoved = copy( $tmp_dir.'/'. basename($_FILES["document"]["tmp_name"]),  $uploadDir . $target_fileName);
+    if ($checkMoved){
+         $postData["type"] = $fileType;
+        //add to database    
+        $db -> sqlite_addDocumentToDb($postData, "files/" . $target_fileName);
+
+        //all is well
+        echo $target_fileName.' was uploaded successfully';
+    }
+    else
+    {
+        echo $tmp_dir . basename($_FILES["document"]["tmp_name"]);
+        echo 'Failed to move uploaded file:'. $target_fileName .'<br>';
+    }
 }
 
+//check if function is set
+if(isset($_GET['function']) && $_GET['function'] === "getDocumentsByWaterbodyId"){
+    
+    $db = new Dbase;
+    
+    echo json_encode($db->sqlite_fetchEntiesByArea($_GET["id"]));
+    
+}
+
+    
 class Dbase {
 
-    const location = "db/phpsqlite.db";
-    const tableName = "documents";
+    private $location = "sqlite:../db/phpsqlite.db";
+    private $tableName = "documents";
 
     // add entry
     public function sqlite_addDocumentToDb($document, $link)
     {
-        $buildTable = "CREATE TABLE IF NOT EXISTS ". self::tableName." (
-        id INTERGER PRIMARY KEY,
-        Date created TEXT,
+        $buildTable = "CREATE TABLE IF NOT EXISTS ". $this->tableName." (
+        id INTEGER PRIMARY KEY,
+        Date created INTEGER,
         Creator TEXT,
         Format TEXT,
         Title TEXT,
@@ -41,54 +88,78 @@ class Dbase {
         Link TEXT
         )";
         
-        sqlite_query($buildTable)
-            
-        $addEntry = "INSERT INTO table1 (
-         Date,
-         Creator,
-         Format,
-         Title,
-         Authors,
-         Project,
-         Waterbody,
-         Link)
-        VALUES
+        $this -> sqlite_query($buildTable);
+        
+        $addEntry = "INSERT INTO ". $this->tableName." (
+             Date,
+             Creator,
+             Format,
+             Title,
+             Authors,
+             Project,
+             Waterbody,
+             Link
+         )
+         VALUES
          (
          ".time().",
-         ".$document['creator'].",
-         ".$document['format'].",
-         ".$document['title'].",
-         ".$document['author'].",
-         ".$document['project'].",
-         ".$document['waterbody'].",
-         ".$document['link'].",
+         '".$this->test_input($document['creator'])."',
+         '".$this->test_input($document['type'])."',
+         '".$this->test_input($document['title'])."',
+         '".$this->test_input($document['author'])."',
+         '".$this->test_input($document['project'])."',
+         '".$this->test_input($document['waterbody'])."',
+         '".$link."'
          )";
-        
-        sqlite_query($addEntry);
+        $this -> sqlite_query($addEntry);
     }
     
     //entries by area
-    public function sqlite_fetchEntryByArea($area)
+    public function sqlite_fetchEntiesByArea($areaID)
     {
-        $query = "SELECT * FROM ".self::tableName." WHERE area='". $area ."'";
-        $result = sqlite_query($query);
-        print_r( json_encode($result) );
+        $query = "SELECT * FROM ".$this->tableName." WHERE Waterbody='". $areaID ."'";
+        $result = $this ->sqlite_query($query);
+        
+        $entries = array();
+
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+/*
+        while ($row = $result->fetch()) {
+
+            extract($row);
+            $entries[] = array(
+                "title" -> $row["Title"],
+                "author" -> $row["Author"],
+                "link" -> $row["Link"],
+                "project" -> $row["Project"],
+            );
+        }
+ */       
+        
+        return $result->fetchAll();
     }
 
     //open database
     private function sqlite_open()
     {
-        $handle = new PDO(self::location) or die("cannot open the database");
+        $handle = new PDO($this->location) or die("cannot open the database");
         return $handle;
     }
 
     //read from database
     private function sqlite_query($query)
     {
-        $array['dbhandle'] = sqlite_open();
-        $array['query'] = $query;
+        $dbhandle = $this->sqlite_open();
         $result = $dbhandle->query($query);
         return $result;
+    }
+    
+    private function test_input($data) 
+    {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
     }
 }
 ?> 
